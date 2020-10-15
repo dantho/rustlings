9e2a3bf728d3d1e6e488d3f3f0696c310d640f10
Dan Thornton <dan.thornton+git@thornton.me>
2020-10-15 00:41:32 -0400

    Through, but not including, traits1.rs

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,7 +1,5 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -12,6 +10,9 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    if num == 0 {0} else {
+        (1..=num).fold(1,|p,v| p*v)
+    }
 }
 
 #[cfg(test)]
@@ -22,6 +23,7 @@ mod tests {
     fn factorial_of_1() {
         assert_eq!(1, factorial(1));
     }
+
     #[test]
     fn factorial_of_2() {
         assert_eq!(2, factorial(2));
@@ -30,5 +32,10 @@ mod tests {
     #[test]
     fn factorial_of_4() {
         assert_eq!(24, factorial(4));
+    }
+
+    #[test]
+    fn factorial_of_100() {
+        assert_eq!(2432902008176640000, factorial(20));
     }
 }
